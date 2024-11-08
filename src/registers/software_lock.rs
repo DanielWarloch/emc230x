ce9c2fab4f9d740ce2c46db7461459a3cd7d616e
Jake Swensen <jake@swensen.io>
2024-11-07 20:28:12 -0600

    feat: add SoftwareLock register

---
+++ src/registers/software_lock.rs
@@ -0,0 +1,16 @@
+use super::RegisterAddress;
+use emc230x_macros::RegisterAddress;
+
+bitfield::bitfield! {
+    #[derive(Clone, Copy, RegisterAddress)]
+    #[register(address = 0xEF)]
+    pub struct SoftwareLock(u8);
+    impl Debug;
+
+    /// Software Lock
+    ///
+    /// 0: All SWL registers are writable.
+    ///
+    /// 1: All SWL registers are read-only. Unlock occurs on power cycle.
+    pub u8, lock, set_lock: 0;
+}
