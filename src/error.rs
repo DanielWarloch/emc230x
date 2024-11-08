270f703033369847f342ec51afa260283f3ba446
Jake Swensen <jake@swensen.io>
2024-11-07 20:39:38 -0600

    rework: remove unused errors

--- src/error.rs
+++ src/error.rs
@@ -11,14 +11,8 @@ pub enum Error {
     #[error("Invalid fan number")]
     InvalidFan,
 
-    #[error("Invalid register")]
-    InvalidRegister,
-
     #[error("Failed to convert register value to specific type")]
     RegisterTypeConversion,
-
-    #[error("Selected fan speed out of range")]
-    SpeedOutOfRange,
 }
 
 impl defmt::Format for Error {
@@ -27,9 +21,7 @@ impl defmt::Format for Error {
             Error::I2c => defmt::write!(f, "I2c"),
             Error::InvalidDeviceId => defmt::write!(f, "InvalidDeviceId"),
             Error::InvalidFan => defmt::write!(f, "InvalidFan"),
-            Error::InvalidRegister => defmt::write!(f, "InvalidRegister"),
             Error::RegisterTypeConversion => defmt::write!(f, "RegisterTypeConversion"),
-            Error::SpeedOutOfRange => defmt::write!(f, "SpeedOutOfRange"),
         }
     }
 }
